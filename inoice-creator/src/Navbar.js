30898497b372b1c2479fd23b6579fd6719d866ae
Mikucki <piotr.mikucki@op.pl>
2022-09-01 15:46:56 -0400

    added navbar, going out of kali changing to windows

---
+++ inoice-creator/src/Navbar.js
@@ -0,0 +1,16 @@
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Modern Box Aparment</h1>
+            <h2>Wybierz pokuj w ktorym masz problem :D</h2>
+            <ul className="nav--room">
+                <li>Salon</li>
+                <li>Kuchnia</li>
+                <li>Sypialnia</li>
+                <li>Lazienka</li>
+            </ul>
+        </nav>
+    )
+}
